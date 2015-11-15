b61d7c52db3ca6c546527581b3925b27c0c8b20a
Kent <kentwong@ucalgary.ca>
2015-11-15 09:43:57 -0700

    carrots problem
    https://open.kattis.com/problems/carrots

---
+++ carrots.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class carrots  { // this is just the main class
+  public static void main(String[] args) { // need this as a method for the actual code
+Scanner sc = new Scanner(System.in); // reads input from scanner
+int i = sc.nextInt(); // next int
+int j = sc.nextInt(); // next int
+System.out.println(j); // print the var; ALSO System.out.println is CASE SENSITIVE!
+  }
+}
